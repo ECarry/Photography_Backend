43bd2599692407edd3c98dd485f81de3c526b680
Administrator <admin@example.com>
2023-03-14 10:28:38 +0800

    变更图片压缩及chaGPT 代码优化

--- api/views.py
+++ api/views.py
@@ -21,8 +21,6 @@ class PhotoViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        print('---------self--------', self.request.data.get('category'))
-
         image_file = serializer.validated_data.pop('image')
         exif_tags = self.get_exif_data(image_file)
         photo = serializer.save(**exif_tags, title=image_file.name)
@@ -30,29 +28,32 @@ class PhotoViewSet(viewsets.ModelViewSet):
         photo.image.save(image_file.name, image_file, save=True)
         photo.thumbnail.save(thumbnail.name, thumbnail, save=True)
 
-    # 从上传的图片获取元数据
+    # chatGPT 优化后的代码
     def get_exif_data(self, image_file):
         exif_tags = {}
         tags = exifread.process_file(image_file, stop_tag='TAG')
         if tags:
-            # 格式化时间
-            timestamp = str(tags.get('EXIF DateTimeOriginal'))
-            timestamp_obj = datetime.strptime(timestamp, "%Y:%m:%d %H:%M:%S")
-            formatted_timestamp = timestamp_obj.strftime("%Y-%m-%d %H:%M:%S")
-            exif_tags['timestamp'] = formatted_timestamp
-            # 格式化光圈环
-            aperture = str(tags.get('EXIF FNumber'))
-            if aperture:
-                exif_tags['aperture'] = self.calc_num(aperture)
-            # 格式化焦距
-            focal_length = str(tags.get('EXIF FocalLengthIn35mmFilm'))
-            if focal_length:
-                exif_tags['focal_length'] = self.calc_num(focal_length)
-            exif_tags['iso'] = str(tags.get('EXIF ISOSpeedRatings'))
-            exif_tags['shutter_speed'] = str(tags.get('EXIF ExposureTime'))
-            exif_tags['camera_brand'] = str(tags.get('Image Make'))
-            exif_tags['camera_model'] = str(tags.get('Image Model'))
-            exif_tags['camera_lens'] = str(tags.get('EXIF LensModel'))
+            fields_to_format = {
+                'EXIF DateTimeOriginal': 'timestamp',
+                'EXIF FNumber': 'aperture',
+                'EXIF FocalLengthIn35mmFilm': 'focal_length',
+                'EXIF ISOSpeedRatings': 'iso',
+                'EXIF ExposureTime': 'shutter_speed',
+                'Image Make': 'camera_brand',
+                'Image Model': 'camera_model',
+                'EXIF LensModel': 'camera_lens'
+            }
+            for tag, field in fields_to_format.items():
+                value = str(tags.get(tag))
+                if value != 'None':
+                    if field == 'timestamp':
+                        timestamp_obj = datetime.strptime(value, "%Y:%m:%d %H:%M:%S")
+                        formatted_timestamp = timestamp_obj.strftime("%Y-%m-%d %H:%M:%S")
+                        exif_tags[field] = formatted_timestamp
+                    elif field == 'aperture' or field == 'focal_length':
+                        exif_tags[field] = self.calc_num(value)
+                    else:
+                        exif_tags[field] = value
 
             lat_tag = tags.get('GPS GPSLatitude')
             lat_ref = tags.get('GPS GPSLatitudeRef')
@@ -66,6 +67,65 @@ class PhotoViewSet(viewsets.ModelViewSet):
                 exif_tags['lat'] = lat
                 exif_tags['lon'] = lon
         return exif_tags
+
+    # 从上传的图片获取元数据
+    # def get_exif_data(self, image_file):
+    #     exif_tags = {}
+    #     tags = exifread.process_file(image_file, stop_tag='TAG')
+    #     if tags:
+    #         # 格式化时间
+    #         timestamp = str(tags.get('EXIF DateTimeOriginal'))
+    #         if timestamp != 'None':
+    #             timestamp_obj = datetime.strptime(timestamp, "%Y:%m:%d %H:%M:%S")
+    #             formatted_timestamp = timestamp_obj.strftime("%Y-%m-%d %H:%M:%S")
+    #             exif_tags['timestamp'] = formatted_timestamp
+    #         else:
+    #             now = datetime.now()  # 获取当前时间
+    #             formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")  # 格式化时间为指定字符串格式
+    #             exif_tags['timestamp'] = formatted_date
+    #
+    #         # 格式化光圈环
+    #         aperture = str(tags.get('EXIF FNumber'))
+    #         if aperture != 'None':
+    #             exif_tags['aperture'] = self.calc_num(aperture)
+    #
+    #         # 格式化焦距
+    #         focal_length = str(tags.get('EXIF FocalLengthIn35mmFilm'))
+    #         if focal_length != 'None':
+    #             exif_tags['focal_length'] = self.calc_num(focal_length)
+    #
+    #         iso = str(tags.get('EXIF ISOSpeedRatings'))
+    #         if iso != 'None':
+    #             exif_tags['iso'] = iso
+    #
+    #         shutter_speed = str(tags.get('EXIF ExposureTime'))
+    #         if shutter_speed != 'None':
+    #             exif_tags['shutter_speed'] = shutter_speed
+    #
+    #         camera_brand = str(tags.get('Image Make'))
+    #         if camera_brand != 'None':
+    #             exif_tags['camera_brand'] = camera_brand
+    #
+    #         camera_model = str(tags.get('Image Model'))
+    #         if camera_model != 'None':
+    #             exif_tags['camera_model'] = camera_model
+    #
+    #         camera_lens = str(tags.get('EXIF LensModel'))
+    #         if camera_lens != 'None':
+    #             exif_tags['camera_lens'] = camera_lens
+    #
+    #         lat_tag = tags.get('GPS GPSLatitude')
+    #         lat_ref = tags.get('GPS GPSLatitudeRef')
+    #         lon_tag = tags.get('GPS GPSLongitude')
+    #         lon_ref = tags.get('GPS GPSLongitudeRef')
+    #
+    #         # Convert the latitude and longitude tags to decimal format
+    #         if lat_tag and lat_ref and lon_tag and lon_ref:
+    #             lat = self.get_decimal_from_dms(lat_tag.values, lat_ref.values)
+    #             lon = self.get_decimal_from_dms(lon_tag.values, lon_ref.values)
+    #             exif_tags['lat'] = lat
+    #             exif_tags['lon'] = lon
+    #     return exif_tags
 
     # 判断获取的数据是否为数字或者 a/b
     def calc_num(self, string):
@@ -91,7 +151,12 @@ class PhotoViewSet(viewsets.ModelViewSet):
     # 创建略缩图
     def create_thumbnail(self, image_file):
         with Image.open(image_file) as img:
-            img.thumbnail((300, 300))
+            # 获取原尺寸等比压缩
+            width, height = img.size
+            aspect_ratio = height / width
+            new_height = int(1080 * aspect_ratio)
+            new_size = (1080, new_height)
+            img.thumbnail(new_size)
             thumbnail_file = io.BytesIO()
             img.save(thumbnail_file, format='JPEG')
             thumbnail_file.name = image_file.name
